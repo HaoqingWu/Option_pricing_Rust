c96cd33e520d60357ae3c3a222fd9ce352bdff7d
Hapqing Wu <wuhaoqing28@gmail.com>
2022-12-05 23:57:07 +0100

    Added examples in main.rs

--- option_pricing/src/main.rs
+++ option_pricing/src/main.rs
@@ -10,7 +10,20 @@ fn main() {
         tau: 1.2,
         S: 80.0,
     };
+
+    let observed_price = European::EuropeanOption{
+        V: Some(option.call_value()),
+        K: 100.0,
+        r: 0.04,
+        vol: None,
+        q: 0.1,
+        tau: 1.2,
+        S: 80.0,
+    };
+    println!("The price of the call option is {}", option.call_value());
     println!("The delta of the option is {}", option.delta());
     println!("The vega of the option is {}", option.vega());
+
+    print!("The implied volatility of the observed option is {}", observed_price.implied_vol());
 
 }
